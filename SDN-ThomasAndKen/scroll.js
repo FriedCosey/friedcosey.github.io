49efca5a3f5da8618e59cb8704e5b0ac2f90cb00
friedcosey <friedcosey@gmail.com>
2017-11-26 02:20:46 +0800

    add debounce

--- SDN-ThomasAndKen/scroll.js
+++ SDN-ThomasAndKen/scroll.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var keyhold = {};
     $("nav a").on("click", function(e){
         e.preventDefault();
         var curUri = $(this).attr("href");
@@ -6,14 +7,32 @@ $(document).ready(function(){
         var scrollId = curUri.substring(scrollIdIndex);
         $("html, body").animate({scrollTop:$("#sdn-"+scrollId).offset().top}, 1000);
     }); 
-
+    
+    $(document).on("mousewheel", debounce(function(e){
+        e.preventDefault();
+            if(e.deltaY < 0){
+                scrolldown();
+            }
+            else if(e.deltaY > 0){
+                scrollup();
+            }
+    
+    },50));
     $(document).on("keydown", function(e){
         e.preventDefault();
-        if(e.keyCode == 40)
+        if(e.keyCode == 40 && keyhold[40] == null){
+            keyhold[40] = true;
             scrolldown();
-        else if(e.keyCode == 38)
+        }
+        else if(e.keyCode == 38 && keyhold[38] == null){
+            keyhold[38] = true;
             scrollup();
-    
+        }
+    });
+
+    $(document).on("keyup", function(e){
+        e.preventDefault();
+        keyhold[e.keyCode] = null;
     });
 
     $('.down').on("click", function(e){
@@ -45,4 +64,20 @@ $(document).ready(function(){
        });
 
     }
+    // Credit to https://davidwalsh.name/javascript-debounce-function
+    function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
 });
